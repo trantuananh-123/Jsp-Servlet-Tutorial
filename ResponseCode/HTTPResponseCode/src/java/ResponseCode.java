9ea99fb8e090a2458d637aa9bf46393f84d260c8
anhtt201 <tom1810.2001@gmail.com>
2022-02-24 23:59:39 +0700

    'HTTP-Response-Code'

---
+++ ResponseCode/HTTPResponseCode/src/java/ResponseCode.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author tom1810-183-59
+ */
+public class ResponseCode extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
+            ServletException, IOException {
+        resp.setContentType("text/html"); //To change body of generated methods, choose Tools | Templates.
+        resp.sendError(401);
+    }
+
+}
